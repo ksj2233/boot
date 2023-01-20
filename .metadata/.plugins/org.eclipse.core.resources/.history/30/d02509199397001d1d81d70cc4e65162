5a272b7c22eb398ae1265aef9a1cd81ad27f970e
sungjin Kim <ksjksb@naver.com>
2023-01-20 16:45:18 +0900

    s

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/30/d02509199397001d1d81d70cc4e65162
@@ -0,0 +1,13 @@
+package com.keduit.bpro01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bpro01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ bpro51/src/main/java/com/keduit/bpro51/Bpro51Application.java
@@ -0,0 +1,13 @@
+package com.keduit.bpro51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bpro51Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bpro51Application.class, args);
+	}
+
+}
